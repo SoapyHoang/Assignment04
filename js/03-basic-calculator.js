3998e691065c00017eec9ba293eab306693a12e8
SoapyHoang <sxhoang@ucsd.edu>
2022-03-15 21:31:29 -0700

    finished assignment

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,6 +1,39 @@
 // IMPORT THE MODULE
+import * as calc from './modules/calculator.js'
 
 // COLLECT NUMBER 1, NUMBER 2, AND OPERATION FROM THE USER
-
 // CHECK TO SEE WHAT OPERATION THEY'RE USING
 // CALL THE APPROPRIATE FUNCTION
+let x, y, calcOperation
+
+do { 
+    x = parseInt(prompt('Enter the first number you want to calculate'))
+    console.log(x)
+
+    y = parseInt(prompt('Enter the second number you want to calculate'))
+    console.log(y)
+
+    calcOperation = prompt('What type of operation do you want to perform? (add, subtract, multiply, divide)')
+    console.log(calcOperation)
+
+    switch(calcOperation) { 
+        case 'add':
+            alert((calc.add(x,y)))
+            break 
+        case 'subtract':
+            alert((calc.subtract(x,y)))
+            break 
+        case 'multiply':
+            alert((calc.multiply(x,y)))
+            break 
+        case 'divide':
+            alert((calc.divide(x,y)))
+            break 
+    }
+
+    
+    if (calcOperation == null || calcOperation != 'add' && calcOperation != 'subtract' && calcOperation != 'multiply' && calcOperation != 'divide')
+    {
+        alert('Please enter add, subtract, multiply, or divide')
+    }
+} while (calcOperation == null || calcOperation != 'add' && calcOperation != 'subtract' && calcOperation != 'multiply' && calcOperation != 'divide')
--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,13 +1,32 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
-
+function calculate(x, y, operator) {
+    switch(operator) {
+        case 'add':
+            return x + y
+        case 'sub':
+            return x - y
+        case 'mul':
+            return x * y
+        case 'div':
+            return x / y
+    }
+}
 // ADD FOUR PUBLIC FUNCTIONS BELOW
-
 // ADD FUNCTION
-
+const add = (x,y) => {
+    return calculate(x, y, 'add')
+}
 // SUBTRACT FUNCTION
-
+const subtract = (x,y) => {
+    return calculate(x, y, 'sub')
+}
 // MULTIPLY FUNCTION
-
+const multiply = (x,y) => {
+    return calculate(x, y, 'mul')
+}
 // DIVIDE FUNCTION
-
+const divide = (x,y) => { 
+    return calculate(x, y, 'div')
+}
 // EXPORT THE FOUR PUBLIC FUNCTIONS
+export{add, subtract, multiply, divide}
